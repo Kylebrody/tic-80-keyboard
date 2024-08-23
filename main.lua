6d1a81d9769af88069f249c108a6c1a49c6ba920
Kylebrody <36281177+Kylebrody@users.noreply.github.com>
2024-08-23 00:06:23 -0500

    Update main.lua
    Added input for going up an octave

--- main.lua
+++ main.lua
@@ -5,8 +5,33 @@ color=0
 id=1
 notepos=math.random(0,95)
 music(0,0,0,0,0)
+c="C-4"
+d="D-4"
+e="E-4"
+f="F-4"
+g="G-4"
+a="A-4"
+b="B-4"
+c2="C-5"
+
+octave=1
 
 function TIC()
+	if keyp(48) then
+		octave=octave+1
+	end
+	
+	if octave==2 then
+		c="C-5"
+		d="D-5"
+		e="E-5"
+		f="F-5"
+		g="G-5"
+		a="A-5"
+		b="B-5"
+		c2="C-6"
+	end
+
 	if btnp(4) then
 		color=color+1
 		notepos=math.random(0,95)
@@ -15,47 +40,48 @@ function TIC()
 	
 	if keyp(17) then
 		color=color+1
-		sfx(id,"C-4", 25)
+		sfx(id,c, 25)
 	end
 	
 	if keyp(23) then
 		color=color+1
-		sfx(id,"D-4", 25)
+		sfx(id,d, 25)
 	end
 	
 	if keyp(05) then
 		color=color+1
-		sfx(id,"E-4", 25)
+		sfx(id,e, 25)
 	end
 	
 	if keyp(18) then
 		color=color+1
-		sfx(id,"F-4", 25)
+		sfx(id,f, 25)
 	end
 	
 	if keyp(20) then
 		color=color+1
-		sfx(id,"G-4", 25)
+		sfx(id,g, 25)
 	end
 	
 	if keyp(25) then
 		color=color+1
-		sfx(id,"A-4", 25)
+		sfx(id,a, 25)
 	end
 	
 	if keyp(21) then
 		color=color+1
-		sfx(id,"B-4", 25)
+		sfx(id,b, 25)
 	end
 	
 	if keyp(09) then
 		color=color+1
-		sfx(id,"C-5", 25)
+		sfx(id,c2, 25)
 	end
 	
 	cls(color)
 	
 	print("Z for rand note",84,50)
 	print("Qwertyui C4 Maj",84,60)
+	print("Spacebar - up octave",84,70)
 	
 end
