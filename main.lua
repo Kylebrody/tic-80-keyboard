d6fb580fa04bb076ca525e20d1503fde300fa582
Kylebrody <36281177+Kylebrody@users.noreply.github.com>
2024-08-24 00:08:35 -0500

    Update main.lua
    added sustain control, 3 synth fx toggles, drum toggle, and more

--- main.lua
+++ main.lua
@@ -1,9 +1,10 @@
 -- version: 0.1
 -- script:  lua
-
+tempo=150
 color=0
-id=1
+id=01
 notepos=math.random(0,95)
+sus=25
 music(0,0,0,0,0)
 --cscale notes
 n1="C-4"
@@ -21,15 +22,55 @@ octave=1
 doctave=0
 coctave=0
 function TIC()
-		
+--phone synth toggle
+	if keyp(62) then--- caps lock
+		id=02
+	end
+--sine wav toggle	
+	if keyp(64) then--- SHIFT
+		id=01
+	end
+--fart/glitch toggle
+	if keyp(63) then--- CTRL
+		id=05
+	end
+---Zany/pitchy
+	if keyp(65) then--- ALT
+		id=06
+	end
+--stop drums toggle	
+	if keyp(14) then --N
+		music()
+	end
+--resume drums toggle
+	if keyp(13) then	 --M
+		music(0,0,0,0,0)
+	end
+--infinite sustain
+	if keyp(19) then	 --S
+		sus=-1
+	end
+--regular sustain
+	if keyp(4) then --D	
+		sus=25
+	end
+--medium sustain
+	if keyp(6) then --D	
+		sus=50
+	end
+--long sustain
+	if keyp(7) then --D	
+		sus=75
+	end
+--switch to Dmaj
 	if keyp(28) then---key1
 		doctave=1
 	end
-	
+--Switch to Cmaj	
 	if keyp(29) then---key2
 		doctave=0
 	end
-	
+--octave cycle	
 	if keyp(48) then----space
 		octave=octave+1
 	end
@@ -208,53 +249,63 @@ end
 	if btnp(4) then
 		color=color+1
 		notepos=math.random(0,95)
-		sfx(id,notepos, 25)
+		sfx(id,notepos, sus)
 	end
 	
 	if keyp(17) then
 		color=color+1
-		sfx(id,n1, 25)
+		sfx(id,n1, sus)
 	end
 	
 	if keyp(23) then
 		color=color+1
-		sfx(id,n2, 25)
+		sfx(id,n2, sus)
 	end
 	
 	if keyp(05) then
 		color=color+1
-		sfx(id,n3, 25)
+		sfx(id,n3, sus)
 	end
 	
 	if keyp(18) then
 		color=color+1
-		sfx(id,n4, 25)
+		sfx(id,n4, sus)
 	end
 	
 	if keyp(20) then
 		color=color+1
-		sfx(id,n5, 25)
+		sfx(id,n5, sus)
 	end
 	
 	if keyp(25) then
 		color=color+1
-		sfx(id,n6, 25)
+		sfx(id,n6, sus)
 	end
 	
 	if keyp(21) then
 		color=color+1
-		sfx(id,n7, 25)
+		sfx(id,n7, sus)
 	end
 	
 	if keyp(09) then
 		color=color+1
-		sfx(id,n8, 25)
+		sfx(id,n8, sus)
 	end
 	
 	cls(color)
+	print("Note Tools: ",150,10,4,false,1,true)
+	print("Z for rand note",150,20,5,false,1,true)
+	print("Qwertyui C4 Maj",150,30,6,false,1,true)
+	print("Spacebar - up octave",150,40,7,false,1,true)
+	print("1=D 2=C N=StopDrums M=Drums",150,50,4,false,1,true)
+	print("fx:",10,10,4,false,1,true)
+	print("caps lck=telephone",20,20,5,false,1,true)
+	print("shift=sinewav",20,30,6,false,1,true)
+	print("cntrl=fart/glitch",20,40,7,false,1,true)
+	print("alt=zany/pitchy",20,50,4,false,1,true)
+	print("S=Infinite sustain",20,60,5,false,1,true)
+	print("D=short sustain",20,70,6,false,1,true)
+	print("F=med sustain",20,80,7,false,1,true)
+	print("G=long sustain",20,90,4,false,1,true)
 	
-	print("Z for rand note",84,50)
-	print("Qwertyui C4 Maj",84,60)
-	print("Spacebar - up octave",84,70)
-	print("1- d root note. 2 - back to C",84,80)
 end
